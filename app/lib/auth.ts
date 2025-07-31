d7c2cbb6ca6e21e70eeefcb453505ca1284582fb
aiqbalsyah <aiqbalsyah@gmail.com>
2025-07-31 20:52:07 +0700

    add trustHost configuration to NextAuth

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -6,6 +6,7 @@ import { prisma } from "./db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  trustHost: true,
   providers: [GitHub, Google],
   callbacks: {
     async session({ session, user }) {
